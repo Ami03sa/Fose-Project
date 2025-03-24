1861f3cbfe3a6c17ea961cd3f1bdf9ff53883862
rmdna <rodrigo-medina@uiowa.edu>
2025-03-24 18:11:47 -0500

    Connecting backend with database

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,6 @@
-# app.py or your main Flask application file
 from flask import Flask
 from flask_mysqldb import MySQL
-from config import Config
+from .config import Config
 from dotenv import load_dotenv
 import os
 
@@ -18,10 +17,13 @@ app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 
 mysql = MySQL(app)  # Initialize MySQL extension
 
-
 @app.route('/')
 def index():
-    return "Flask app with MySQL is running!"
+    # MySQL query inside the route
+    cur = mysql.connection.cursor()
+    cur.execute('''SELECT * FROM users''')  # You can replace 'some_table' with any table in your database
+    results = cur.fetchall()
+    return str(results)
 
 @app.route('/users')
 def get_users():
@@ -29,7 +31,6 @@ def get_users():
     cursor.execute('SELECT * FROM users')  # Example query
     users = cursor.fetchall()  # Fetch all records
     return {'users': users}  # Return users as JSON (just an example)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
