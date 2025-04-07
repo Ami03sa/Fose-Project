05afa1994e64460540317ce3074450b291ea684f
rmdna <rodrigo-medina@uiowa.edu>
2025-04-07 16:45:48 -0500

    Added new branch

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Login from "./components/Login";
-import Dashboard from "./components/Dashboard";
+import Login from "./components/login";
+// import Dashboard from "./components/Dashboard";
 import Request from "./components/Request";
-import Signup from "./components/Signup";
+import Signup from "./components/signup";
 import Respond from "./components/Respond"; 
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        {/* <Route path="/dashboard" element={<Dashboard />} /> */}
         <Route path="/request" element={<Request />} />
         <Route path="/respond" element={<Respond />} />
       </Routes>
